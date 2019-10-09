1f6ac9245e034695c2b5a0c055cb30cda461a6f2
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-09 13:04:53 -0700

    axios now working, needed to specify key in the get link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios"
+
+
 
 function App() {
+  const [pictureDay, setPictureDay] = useState("PLACEHOLDER");
+
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log("Play that funky music", err)
+  })
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+       
       </p>
     </div>
   );

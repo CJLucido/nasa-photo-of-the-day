fa77948c91e36235f557efe8391e2905d369085b
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-09 16:19:21 -0700

    even better formed style

--- src/components/Atoms/ImgBox.js
+++ src/components/Atoms/ImgBox.js
@@ -3,8 +3,8 @@ import React from "react"
 function ImgBox(props){
 
     return(
-        <div className="Img-box-container">
-        <img src={props.path} alt={props.detail}/>
+        <div >
+        <img className="Img-box-container" src={props.path} alt={props.detail}/>
         </div>
         //input src={props.path} alt={props.detail}
     )
